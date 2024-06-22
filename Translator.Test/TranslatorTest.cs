12975234a6e351c5edc8e2177affa2cd081e65d9
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-06-21 20:09:57 -0400

    add more test cases with output result

--- Translator.Test/TranslatorTest.cs
+++ Translator.Test/TranslatorTest.cs
@@ -1,18 +1,48 @@
-﻿namespace Translator.Test;
+﻿using Xunit.Abstractions;
+
+namespace Translator.Test;
 
 public class TranslatorTest
 {
-    [Fact]
-    public void TestTranslation()
-    {
-        var translator = new Translator();
+    private ITestOutputHelper output;
+    private Translator translator = new();
 
+    public TranslatorTest(ITestOutputHelper output)
+    {
+        this.output = output;
         translator.AddWord("hello", "hola");
         translator.AddWord("world", "mundo");
+    }
 
-        var input = "Hello, world!";
-        var output = translator.Translate(input);
+    [Fact]
+    public void TranslateSingleWord()
+    {
+        var result = translator.Translate("hello");
+        Assert.Equal("hola", result);
+        output.WriteLine(result);
+    }
 
-        Assert.Equal("Hola, mundo!", output);
+    [Fact]
+    public void TranslateLowerCaseSentence()
+    {
+        var result = translator.Translate("hello world");
+        Assert.Equal("hola mundo", result);
+        output.WriteLine(result);
+    }
+
+    [Fact]
+    public void TranslateTitleCaseSentence()
+    {
+        var result = translator.Translate("Hello World");
+        Assert.Equal("Hola Mundo", result);
+        output.WriteLine(result);
+    }
+
+    [Fact]
+    public void TranslateSentenceWithPunctuation()
+    {
+        var result = translator.Translate("Hello, world!");
+        Assert.Equal("Hola, mundo!", result);
+        output.WriteLine(result);
     }
 }
